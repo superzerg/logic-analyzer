661845ea798c1d057017aba7dda7bdc9e6879816
superzerg <z3r9l1n9@gmail.com>
2013-03-11 21:40:54 +0000

    add Load() and Save() documentation

--- search/functions_6c.js
+++ search/functions_6c.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['load',['Load',['../classlogic__input.html#a6432c8ce43a33c26d18f9e58d392aa20',1,'logic_input']]],
+  ['load',['Load',['../classlogic__input.html#a6432c8ce43a33c26d18f9e58d392aa20',1,'logic_input::Load()'],['../classprotocol.html#a8dfce83140a9f27fe162678f8b8f8533',1,'protocol::Load()']]],
   ['logic_5finput',['logic_input',['../classlogic__input.html#a450e489e87441db0bad7c5c9d71494b5',1,'logic_input']]]
 ];
--- search/functions_73.js
+++ search/functions_73.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['save',['Save',['../classlogic__input.html#a2a9a9dc41ce8ff467cf9aef67276d56f',1,'logic_input']]],
+  ['save',['Save',['../classlogic__input.html#a2a9a9dc41ce8ff467cf9aef67276d56f',1,'logic_input::Save()'],['../classprotocol.html#aa25a252754bae008d40ec79a0062fd69',1,'protocol::Save()']]],
   ['sprint',['sprint',['../classbinary.html#a4c25558b865bfbc5e3d7ca405d0d3b6f',1,'binary']]]
 ];
